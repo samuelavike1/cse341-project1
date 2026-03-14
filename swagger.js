15e5db1d4854c4731a53fec6e16bd1518021d7ff
Samuel Avike <samuelavike@gmail.com>
2026-03-14 22:04:35 +0000

    swaggger

--- swagger.js
+++ swagger.js
@@ -13,6 +13,16 @@ const doc = {
         title: 'CSE 341 Project 1 - CONTACTS API',
         description: 'A simple API to manage contacts, allowing users to create, read, update, and delete contact information. This API is built using Node.js and Express, and it uses MongoDB for data storage.',
     },
+    servers: [
+        {
+            url: 'https://cse341-project1-sczx.onrender.com',
+            description: 'Production server'
+        },
+        {
+            url: 'http://localhost:3000',
+            description: 'Local development'
+        }
+    ]
 };
 
 const outputFile = './swagger-output.json';
